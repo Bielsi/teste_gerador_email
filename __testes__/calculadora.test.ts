fa27481b83d67126c302c5e7f3a521f049fcb5a3
Bielsi <gmmota2701@gmail.com>
2024-06-13 22:02:07 -0300

    final_teste_email

--- __testes__/calculadora.test.ts
+++ __testes__/calculadora.test.ts
@@ -1,5 +1,17 @@
-import {generateRandomGmailEmail} from "../src/calculadora";
+import {generateRandomGmailEmail, getemails} from "../src/calculadora";
 
+  describe("Gerador de email", () => {
     test(" Lucas@gmail.com ", () => {
         expect(generateRandomGmailEmail("Lucas", "gmail")).toBe("Lucas@gmail.com");
     });
+
+    test(" criar 10 emails", () => {
+        expect(getemails(10).length).toBe(10);
+    });
+
+    test(" criar 5 emails", () => {
+        expect(getemails(5).length).toBe(5);
+    });
+    
+
+  });
--- src/calculadora.ts
+++ src/calculadora.ts
@@ -1,18 +1,5 @@
-export class email {}
-function getRandomInt(min: number, max: number): number {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  
-  function getRandomString(length: number): string {
-    const characters = 'abcdefghijklmnopqrstuvwxyz';
-    let result = '';
-    for (let i = 0; i < length; i++) {
-      const randomIndex = getRandomInt(0, characters.length - 1);
-      result += characters[randomIndex];
-    }
-    return result;
-  }
-  
+export class Email {}
+
   export function generateRandomGmailEmail(name: string, server: string): string {
     const emailName = name; // Tamanho da parte do nome do email
     const gmailDomain = server; // Domínio fixo do Gmail
@@ -24,4 +11,30 @@ function getRandomInt(min: number, max: number): number {
     return email;
   }
   
+  function getRandomInt(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  function getRandomString(length: number): string {
+    const characters = 'abcdefghijklmnopqrstuvwxyz';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+      const randomIndex = getRandomInt(0, characters.length - 1);
+      result += characters[randomIndex];
+    }
+    return result;
+  }
+  
+  export function getemails(qtd: number): string[] {
+    let result: string[] = [];
+    for (let i = 0; i < qtd; i++) {
+      let emailname = getRandomString(6);
+      let email = emailname + "@gmail.com"
+      result.push(email);
+    }
+    console.log(result);
+    return result;
+    
+  }
+
   console.log(generateRandomGmailEmail('lucas', 'gmail'));  
